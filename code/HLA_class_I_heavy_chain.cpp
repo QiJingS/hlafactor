82787321808d971bbe20eead26f42bbeb52291c8
QiJingS <qijing.shen@reuben.ox.ac.uk>
2026-05-22 11:37:58 +0100

    data_input

--- code/HLA_class_I_heavy_chain.cpp
+++ code/HLA_class_I_heavy_chain.cpp
@@ -48,6 +48,82 @@ std::string resolve_nk_lookup_key(
     return best_key;
 }
 
+std::string nk_category_from_allele(const std::string& allele)
+{
+    auto star_pos = allele.find('*');
+    if (star_pos == std::string::npos) {
+        return "";
+    }
+
+    std::string locus = allele.substr(0, star_pos);
+    if (locus == "HLA_A" || locus == "HLA_B" || locus == "HLA_C") {
+        return locus;
+    }
+
+    return "";
+}
+
+std::string normalize_nk_ligand(const std::string& ligand)
+{
+    std::string normalized = ligand;
+
+    for (size_t pos = normalized.find("Bw");
+         pos != std::string::npos;
+         pos = normalized.find("Bw", pos + 2)) {
+        normalized.replace(pos, 2, "BW");
+    }
+
+    return normalized;
+}
+
+bool allele_prefix_match(const std::string& lhs, const std::string& rhs)
+{
+    const std::string& shorter =
+        lhs.size() <= rhs.size() ? lhs : rhs;
+    const std::string& longer =
+        lhs.size() <= rhs.size() ? rhs : lhs;
+
+    return longer.size() > shorter.size() &&
+           longer.compare(0, shorter.size(), shorter) == 0 &&
+           longer[shorter.size()] == ':';
+}
+
+void add_ligand_call(
+    std::vector<std::string>& ligands,
+    const std::string& ligand,
+    int dosage)
+{
+    for (int d = 0; d < dosage; ++d) {
+        ligands.push_back(ligand);
+    }
+}
+
+std::string format_ligands(std::vector<std::string> ligands)
+{
+    if (ligands.empty()) {
+        return "NA";
+    }
+
+    std::sort(
+        ligands.begin(),
+        ligands.end(),
+        [](const std::string& lhs, const std::string& rhs) {
+            if (lhs == ".") return false;
+            if (rhs == ".") return true;
+            return lhs < rhs;
+        });
+
+    std::string out;
+    for (const auto& ligand : ligands) {
+        if (!out.empty()) {
+            out += "/";
+        }
+        out += ligand;
+    }
+
+    return out;
+}
+
 }
 
 std::vector<sample_variants_NK> calculate_v_NK(int argc, char* argv[])
@@ -86,7 +162,10 @@ std::vector<sample_variants_NK> calculate_v_NK(int argc, char* argv[])
         std::getline(ss, category, '\t');
         std::getline(ss, ligand, '\t');
 
-        nk_lookup[normalize_nk_table_allele(allele)] = {category, ligand};
+        nk_lookup[normalize_nk_table_allele(allele)] = {
+            category,
+            normalize_nk_ligand(ligand)
+        };
     }
 
     for (const auto& s : samples)
@@ -96,6 +175,7 @@ std::vector<sample_variants_NK> calculate_v_NK(int argc, char* argv[])
         std::vector<std::string> var_name;
         std::vector<std::string> genotypes;
 
+        std::vector<std::string> a_ligands;
         std::vector<std::string> c_ligands;
         std::vector<std::string> b_ligands;
 
@@ -130,12 +210,47 @@ std::vector<sample_variants_NK> calculate_v_NK(int argc, char* argv[])
         }
 
         std::unordered_map<std::string, int> matched_dosage;
+        std::unordered_map<std::string, std::pair<std::string, int>> unmatched_dosage;
 
-        for (const auto& [allele, dosage] : normalized_dosage)
+        std::vector<std::pair<std::string, int>> allele_dosages(
+            normalized_dosage.begin(),
+            normalized_dosage.end());
+        std::sort(
+            allele_dosages.begin(),
+            allele_dosages.end(),
+            [](const auto& lhs, const auto& rhs) {
+                if (lhs.first.size() != rhs.first.size()) {
+                    return lhs.first.size() < rhs.first.size();
+                }
+                return lhs.first < rhs.first;
+            });
+
+        for (const auto& [allele, dosage] : allele_dosages)
         {
             std::string lookup_key = resolve_nk_lookup_key(allele, nk_lookup);
             if (lookup_key.empty())
             {
+                std::string category = nk_category_from_allele(allele);
+                if (category.empty()) {
+                    continue;
+                }
+
+                std::string unmatched_key = allele;
+                for (const auto& [existing_key, value] : unmatched_dosage) {
+                    (void)value;
+                    if (allele == existing_key ||
+                        allele_prefix_match(allele, existing_key)) {
+                        unmatched_key = existing_key;
+                        break;
+                    }
+                }
+
+                auto current = unmatched_dosage.find(unmatched_key);
+                if (current == unmatched_dosage.end() || dosage > current->second.second)
+                {
+                    unmatched_dosage[unmatched_key] = {category, dosage};
+                }
+
                 continue;
             }
 
@@ -156,58 +271,48 @@ std::vector<sample_variants_NK> calculate_v_NK(int argc, char* argv[])
             const std::string& ligand =
                 it->second.second;
 
-            for (int d = 0; d < dosage; ++d)
+            if (category == "HLA_A")
             {
-                if (category == "HLA_C")
-                {
-                    c_ligands.push_back(ligand);
-                }
-                else if (category == "HLA_B")
-                {
-                    b_ligands.push_back(ligand);
-                }
+                add_ligand_call(a_ligands, ligand, dosage);
+            }
+            else if (category == "HLA_C")
+            {
+                add_ligand_call(c_ligands, ligand, dosage);
+            }
+            else if (category == "HLA_B")
+            {
+                add_ligand_call(b_ligands, ligand, dosage);
             }
         }
 
+        for (const auto& [allele, value] : unmatched_dosage)
+        {
+            (void)allele;
+            const std::string& category = value.first;
+            int dosage = value.second;
+
+            if (category == "HLA_A")
+            {
+                add_ligand_call(a_ligands, ".", dosage);
+            }
+            else if (category == "HLA_C")
+            {
+                add_ligand_call(c_ligands, ".", dosage);
+            }
+            else if (category == "HLA_B")
+            {
+                add_ligand_call(b_ligands, ".", dosage);
+            }
+        }
+
+        var_name.push_back("HLA_A_ligand");
+        genotypes.push_back(format_ligands(a_ligands));
+
         var_name.push_back("HLA_C_ligand");
-
-        if (c_ligands.size() == 1)
-        {
-            genotypes.push_back(c_ligands[0]);
-        }
-        else if (c_ligands.size() == 2)
-        {
-            std::sort(
-                c_ligands.begin(),
-                c_ligands.end());
-
-            genotypes.push_back(
-                c_ligands[0] + "/" + c_ligands[1]);
-        }
-        else
-        {
-            genotypes.push_back("NA");
-        }
+        genotypes.push_back(format_ligands(c_ligands));
 
         var_name.push_back("HLA_B_ligand");
-
-        if (b_ligands.size() == 1)
-        {
-            genotypes.push_back(b_ligands[0]);
-        }
-        else if (b_ligands.size() == 2)
-        {
-            std::sort(
-                b_ligands.begin(),
-                b_ligands.end());
-
-            genotypes.push_back(
-                b_ligands[0] + "/" + b_ligands[1]);
-        }
-        else
-        {
-            genotypes.push_back("NA");
-        }
+        genotypes.push_back(format_ligands(b_ligands));
 
         def_NK.push_back({
             sample_id,
